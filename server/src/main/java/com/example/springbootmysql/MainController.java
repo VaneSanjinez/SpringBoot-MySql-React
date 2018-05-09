1b3eee90adb228a456196023ce68314d479106be
VaneSanjinez <vane.sanji@gmail.com>
2018-05-09 18:46:49 -0400

    Changed getMapping with RequestMapping and defined methods, delete, update get and post

--- server/src/main/java/com/example/springbootmysql/MainController.java
+++ server/src/main/java/com/example/springbootmysql/MainController.java
@@ -4,39 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
-@Controller // class is a controller
+@RestController // class is a controller
 @RequestMapping(path = "/demo") // URL start with demo
 public class MainController {
-    @Autowired //repository (bean)
+
     private UserRepository userRepository;
-
-    @GetMapping(path = "/add") //Map get Request
-
-    public @ResponseBody String addNewUser(@RequestParam String name , @RequestParam Integer age){
-        User n = new User();
-        n.setName(name);
-        n.setAge(age);
-
-        userRepository.save(n);
-        return "saved";
+    @Autowired //repository (bean)
+    public MainController(UserRepository userRepository){
+        this.userRepository = userRepository;
     }
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<User> getAllUsers(){
+
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<User> getAll(){
         return userRepository.findAll();
     }
 
-    @RequestMapping(value = "/user/{id}")
-    public Optional<User> getSingleUser(@PathVariable Integer id){
-        //return userRepository.findById(Long.valueOf(id)).get();
-        return  userRepository.findById(id);
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public List<User> create (@RequestBody User user){
+        userRepository.save(user);
+
+        return userRepository.findAll();
     }
-    @DeleteMapping(path = "/delete/{id}")
-    public @ResponseBody Iterable<User> deleteUser(@PathVariable("id") Integer id){
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public  List<User> delete (@PathVariable long id){
         userRepository.deleteById(id);
 
         return userRepository.findAll();
     }
-
+    @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
+    public User getUserById (@PathVariable long id){
+        return userRepository.findById(id).get();
+    }
 }
