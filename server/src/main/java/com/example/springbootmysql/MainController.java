684bdb75365d340448d492c66006dd1389c73de5
Vanessa Sanjinez <vane.sanji@gmail.com>
2018-05-09 10:51:12 -0400

    Added getSingleUser and deleteUser functions, problems with pathVariable long and int

--- server/src/main/java/com/example/springbootmysql/MainController.java
+++ server/src/main/java/com/example/springbootmysql/MainController.java
@@ -2,10 +2,9 @@ package com.example.springbootmysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller // class is a controller
 @RequestMapping(path = "/demo") // URL start with demo
@@ -25,6 +24,18 @@ public class MainController {
     }
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "/user/{id}")
+    public Optional<User> getSingleUser(@PathVariable Integer id){
+        //return userRepository.findById(Long.valueOf(id)).get();
+        return  userRepository.findById(id);
+    }
+    @DeleteMapping(path = "/delete/{id}")
+    public @ResponseBody Iterable<User> deleteUser(@PathVariable("id") Integer id){
+        userRepository.deleteById(id);
+
         return userRepository.findAll();
     }
 
